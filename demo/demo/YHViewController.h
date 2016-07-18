b1744dd8e3de0b4c8decd089ccf5aaef307daa95
优果keji <youguokeji@kingdemacbook-air.local>
2016-07-18 22:13:33 +0800

    加个类

---
+++ demo/demo/YHViewController.h
@@ -0,0 +1,13 @@
+//
+//  YHViewController.h
+//  demo
+//
+//  Created by 优果keji on 16/7/18.
+//  Copyright © 2016年 KNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHViewController : UIViewController
+
+@end
---
+++ demo/demo/YHViewController.m
@@ -0,0 +1,37 @@
+//
+//  YHViewController.m
+//  demo
+//
+//  Created by 优果keji on 16/7/18.
+//  Copyright © 2016年 KNG. All rights reserved.
+//
+
+#import "YHViewController.h"
+
+@interface YHViewController ()
+
+@end
+
+@implementation YHViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
